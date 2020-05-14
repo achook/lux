8a95c9fceaef1571bafb19bf4430df12d0a12d33
achook <karol@kucza.xyz>
2020-05-14 23:00:56 +0200

    First WIP version

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>LED</h1>
+      <div class="buttons">
+        <button @click="set(1)">Kolor</button>
+        <button @click="set(2)">Miganie</button>
+        <button @click="set(3)">Fala</button>
+        <button @click="set(4)">Cykl</button>
+        <button @click="set(0)">Wyłącz</button>
+      </div>
+      <ColorPicker @change="updateColor" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ColorPicker from './components/ColorPicker'
+
+const MQTT_URL = 'malinka.local'
+const MQTT_PORT = 9001
+const MQTT_DEVICE = 'lux'
+const MQTT_USER = 'sezamek'
+const MQTT_PASSWORD = 'kANQcGkgeg6rlZnk'
+const MQTT_CHANNEL = 'test'
+
+const mqtt = require('paho-mqtt')
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    ColorPicker
+  },
+
+  data () {
+    return {
+      client: null,
+      connected: false,
+      color: 'FFFFFF'
+    }
+  },
+
+  created () {
+    this.client = new mqtt.Client(MQTT_URL, MQTT_PORT, '', MQTT_DEVICE)
+    this.client.connect({
+      userName: MQTT_USER,
+      password: MQTT_PASSWORD,
+      onSuccess: () => { this.connected = true },
+      onFailure: (context, code, message) => { this.throwError(message) }
+    })
+  },
+
+  methods: {
+    set (code) {
+      if (!this.connected) return
+
+      const message = new mqtt.Message(code.toString())
+      message.destinationName = MQTT_CHANNEL
+      this.client.send(message)
+    },
+
+    updateColor (color) {
+      this.color = color
+    },
+
+    throwError (message) {
+      console.log(message)
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input v-model="color" @change="emitChange" type="color"/>
+    <p> {{ color }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorPicker',
+
+  data () {
+    return {
+      color: '#ff0000'
+    }
+  },
+
+  mounted () {
+    this.emitChange()
+  },
+
+  methods: {
+    emitChange () {
+      const color = this.color.substr(1).toUpperCase()
+      this.$emit('change', color)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
